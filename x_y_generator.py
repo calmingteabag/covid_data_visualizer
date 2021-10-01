b60fd7e5ef35c9a19af1a1ac4c3ff09384d60d1b
badcompwaifu <phasedkamo@gmail.com>
2021-10-01 05:07:59 -0300

    Minor fixes: remove unnecessary comments and blank space

--- x_y_generator.py
+++ x_y_generator.py
@@ -1,12 +1,14 @@
 from process_csv import ProcessCsv
 import pandas as pd
 
+
 class Csvgraphs(ProcessCsv):
 
-    def __init__(self, index_column, file=None, x_axis=None, y_axis=None, values=None):
-        super().__init__(index_column, file, x_axis)
+    def __init__(self, index_column, file=None, x_axis=None, y_axis=None, values=None, separator=None):
+        super().__init__(index_column, file, x_axis, separator)
         self.y_axis = y_axis
         self.values = values
+        self.separator = separator
 
     def generate_x_y(self):
         dataframe = ProcessCsv.csv_slicer(self)
@@ -14,11 +16,3 @@ class Csvgraphs(ProcessCsv):
         extract_y = dataframe.loc[self.values, self.y_axis]
 
         return extract_x, extract_y
-
-
-# example: gen time graph for all 'gol' values using 'values_a' column in test_csv
-mygraph = Csvgraphs('name', "C:\\Users\\Kamo\\PycharmProjects\\covid_data_visualizer\\data\\test_csv.CSV",
-                    'time', 'values_a', 'gol')
-
-x = mygraph.generate_x_y()
-print(x)
