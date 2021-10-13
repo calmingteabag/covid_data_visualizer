2b4bf45a58f62c8f4818a5c05d5cd8c80e98c98f
badcompwaifu <phasedkamo@gmail.com>
2021-10-13 20:34:23 -0300

    Fixed class constructor, now it works with mysql cursor

--- sqlizator.py
+++ sqlizator.py
@@ -1,4 +1,7 @@
 import mysql.connector as msqlc
+from json_x_y_generator import GraphConstructor
+import json
+import urllib.request
 
 '''
 This class creates and modifies a single SQL table.
@@ -22,25 +25,24 @@ class SqlOperator:
 
     def __init__(
             self,
-            host,
-            user,
-            password,
-            dbase,
-            cursor_name,
-            connector_name,
-            country,
-    ):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.dbase = dbase
-        self.cursor_name = cursor_name
-        self.connector_name = connector_name
-        self.country = country
+            table_name=None,
+            dbase=msqlc.connect(
+                host='127.0.0.1',
+                user='root',
+                password='12345',
+                database='kamodb'
+            ),
 
+    ):
+        self.table_name = table_name
+        self.dbase = dbase
+        self.cursor = dbase.cursor()
+        self.datalist = []
+
+    # operator functions
     def create_table(self):
 
-        self.cursor_name.execute(
+        self.cursor.execute(
             'CREATE TABLE IF NOT EXISTS {} ( \
                 covid_id INT AUTO_INCREMENT PRIMARY KEY, \
                 covid_date VARCHAR(10), \
@@ -50,23 +52,35 @@ class SqlOperator:
                 total_deaths VARCHAR(10), \
                 new_deaths VARCHAR(10), \
                 new_deaths_smoothed VARCHAR(10) \
-            )'.format(self.country)
+            )'.format(self.table_name)
         )
 
-    def insert_data(self, column_name, data):
+    def get_data(self, country, get_values):
 
-        self.cursor_name.execute(
-            'INSERT IGNORE INTO {}({}) VALUES ({}) '.format(self.country, column_name, data)
-        )
+        with urllib.request.urlopen("https://covid.ourworldindata.org/data/owid-covid-data.json") as url:
+            data = json.loads(url.read().decode())
+
+        process_data = data.get(country, {}).get('data')
+
+        for values in process_data:
+            temp_values = values.get(get_values)
+            self.datalist.append(temp_values)
+
+    def insert_data(self, column_name):
+        values = [list([item]) for item in self.datalist]
+        # params = f'INSERT IGNORE INTO {self.country} ({col}) VALUES (%s)'
+        self.cursor.executemany(u'INSERT IGNORE INTO {} ({}) VALUES (%s)'.format(self.table_name, column_name), values)
+        # self.cursor.execute('INSERT INTO {} ({}) VALUES ({})'.format(self.table_name, column_name, values))
+        self.dbase.commit()
 
     def del_data(self, column_name, data):
 
-        self.cursor_name.execute(
-            'DELETE FROM {} WHERE {} = {}'.format(self.country, column_name, data)
+        self.cursor.execute(
+            'DELETE FROM {} WHERE {} = {}'.format(self.table_name, column_name, data)
         )
 
     def update_data(self, column_name, data, cond_column, cond_value):
 
-        self.cursor_name.execute(
-            'UPDATE {} SET {} = {} WHERE {} = {} '.format(self.country, column_name, data, cond_column, cond_value)
+        self.cursor.execute(
+            'UPDATE {} SET {} = {} WHERE {} = {} '.format(self.table_name, column_name, data, cond_column, cond_value)
         )
